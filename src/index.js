84337e1c24ed5a33653b6b92dad8b29417bb62ef
Devin Udy <34194818+Devin-The-Dev@users.noreply.github.com>
2023-05-18 14:06:05 -0600

    Created first component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>An intro to React</h1>
+    {/* Lets create our first component */}
+    {/* This 'greeting attribute is what React calls a prop (short for 'property') */}
+    <Hello greeting={'Hello' + 'React Ninja'}/>
+
     <App />
   </React.StrictMode>
 );
