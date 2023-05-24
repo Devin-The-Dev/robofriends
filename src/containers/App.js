761567c99ee5fbf6f01ed470b8e06fd74ed18d1a
Devin Udy <34194818+Devin-The-Dev@users.noreply.github.com>
2023-05-24 17:40:28 -0600

    Converted class component to React hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 // Add created components
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
@@ -7,57 +7,49 @@ import ErrorBoundary from '../components/ErrorBoundary';
 // Import the new font
 import './App.css';
 
-class App extends Component {
-   
-    constructor() {
-        super();
-        // This is what we call states. They're objects that describes an application. Things inside of this are things that can change
-        // These changes are done by using the 'class' and 'this' keywords
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
+    
+    // We're going to create a hook
+    // Let's have 'robots be our state, and setRobots to be out function to change the robots
+    // This seems to be much like a shorthand way for a getter and setter
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    const [count, setCount] = useState(0);
 
-    // We're going to use componentDidMount() to make requests to the JSONPlaceholder API. 
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
-    }
+            .then(users => setRobots(users));
+        // There needs to be an array (even empty) in order to prevent uncessassary fetchings
+    }, [count]) //Only run if count changes
 
     // This collects input from the user
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
-    
-    // This is used to, well, render the information 
-    render(){
-        // Destructure to make our code cleaner
-        const { robots, searchfield } = this.state;
 
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
 
-        // This in case JSONPlaceholder API is getting a lot of traffic
-        // We only want the 'loading' to appear iff the length is 0. Well, since 0 also means false, let's just make 0 the true statement
-        return !robots.length ?
-            <h1>Loading ...</h1> :
-            (
-                <React.StrictMode>
-                    <div className='tc'>
-                        <h1 className='f1'>RoboFriends</h1>
-                        <SearchBox searchChange={this.onSearchChange}/>
-                        <Scroll>
-                            <ErrorBoundary>
-                                <CardList robots={ filteredRobots } />
-                            </ErrorBoundary>
-                        </Scroll>
-                    </div>
-                </React.StrictMode>
-            )
-    }
+    // This first part is in case JSONPlaceholder API is getting a lot of traffic
+    // We only want the 'loading' to appear iff the length is 0. Well, since 0 also means false, let's just make 0 the true statement
+    return !robots.length ?
+        <h1>Loading ...</h1> :
+        (
+            <React.StrictMode>
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <button onClick={() =>  setCount(count + 1) }>Click Me!</button>
+                    <SearchBox searchChange={onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={ filteredRobots } />
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            </React.StrictMode>
+        )
 
     // When the page loads it'll run constructor, render, componentDidMount, then render again. This according to React's documentation
 }
